f712e5fb762343c7c84f8559cad919dd0c7835ff
pecoBr <rafaelgamer321tosta@gmail.com>
2024-11-29 15:36:46 -0300

    terminando o front-end

--- src/Pages/Doados/Doados.jsx
+++ src/Pages/Doados/Doados.jsx
@@ -1,9 +1,18 @@
-
+import S from './doados.module.scss'
+import livro from '../../assets/livro.png'
 
 export default function Doados() {
   return (
-  <section>
-    <h2>Doados</h2>
+  <section className={S.boxDoados}>
+    <h2>Livros Doados</h2>
+    <section className={S.boxCard}>
+      <article>
+        <img src={livro} alt="" />
+        <h3>O Protagonista</h3>
+        <p>Susanne Andrade</p>
+        <p>Ficção</p>
+      </article>
+    </section>
   </section>
   )
 }
